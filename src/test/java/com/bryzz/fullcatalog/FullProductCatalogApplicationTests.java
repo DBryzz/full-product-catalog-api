adf1d3ddbec3109cad2a26f48c277f970310512e
Bryzz <DBryzz>
2019-10-24 18:43:41 +0100

    error checked

--- src/test/java/com/bryzz/fullcatalog/FullProductCatalogApplicationTests.java
+++ src/test/java/com/bryzz/fullcatalog/FullProductCatalogApplicationTests.java
@@ -1,12 +1,12 @@
 package com.bryzz.fullcatalog;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FullProductCatalogApplicationTests {
 
-	@Test
+	@org.junit.Test
 	void contextLoads() {
 	}
 
